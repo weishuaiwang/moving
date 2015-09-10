5f7a9c41c8f09a2ffa2f9f9a6365cefdba68495a
Vincent <ws.file@gmail.com>
2015-09-10 17:51:27 +0800

    temp data

---
+++ data/runtime/Data/site_options.php
@@ -0,0 +1,1 @@
+a:10:{s:9:"site_name";s:10:"Moving CMS";s:9:"site_host";s:17:"http://kanpu.dev/";s:9:"site_root";s:0:"";s:8:"site_icp";s:0:"";s:16:"site_admin_email";s:15:"ws.file@163.com";s:11:"site_tongji";s:0:"";s:14:"site_copyright";s:0:"";s:14:"site_seo_title";s:10:"Moving CMS";s:17:"site_seo_keywords";s:0:"";s:20:"site_seo_description";s:10:"Moving CMS";}
